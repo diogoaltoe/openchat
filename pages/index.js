66a0466e6697e27915ab168a83782ef761b308c7
Diogo <diogoaltoe@gmail.com>
2023-06-08 00:50:38 +0000

    - Initial config for NextJS. - Added index page.

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <>
+            <h1>OpenChat</h1>        
+            <h4>The place to share and discuss ideas.</h4>
+        </>
+    )
+}
+
+export default Home;
